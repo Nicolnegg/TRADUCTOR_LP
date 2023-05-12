d449ff0aa54795797f52e0969aec7df3a4efe788
Manuelma4 <mmayaro@unal.edu.co>
2023-05-12 07:55:14 -0500

    Se arreglo el and y el or

--- src/MetodosTraductor.java
+++ src/MetodosTraductor.java
@@ -421,11 +421,11 @@ public class MetodosTraductor implements MiLenguajeListener {
         }
 
         else if(ctx.Or()!=null){
-            System.out.print(" Or ");
+            System.out.print(" or ");
         }
 
         else if(ctx.And()!=null){
-            System.out.print(" And ");
+            System.out.print(" and ");
         }
 
 
